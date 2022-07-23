445b09d2be55e1a68ada47051c201d6246465d73
TETADI <tejeswar.tadi1@gmail.com>
2022-07-23 15:01:45 +0530

    More print's

--- Try/src/Pop.java
+++ Try/src/Pop.java
@@ -5,6 +5,19 @@ public class Pop {
 	{
 		// TODO Auto-generated method stub
 System.out.println("Hai");
+System.out.println("Hai");
+
+System.out.println("Hai");
+
+System.out.println("Hai");
+
+System.out.println("Hai");
+System.out.println("Hai");
+System.out.println("Hai");
+System.out.println("Hai");
+System.out.println("Hai");
+
+
 	}
 
 }
