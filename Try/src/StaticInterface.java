63fadb29b1b4d8fd9297dc0732744c58321da1b5
TETADI <tejeswar.tadi1@gmail.com>
2022-07-28 06:48:52 +0530

    Statsic Interface

--- Try/src/StaticInterface.java
+++ Try/src/StaticInterface.java
@@ -1,8 +1,19 @@
+interface Maddy
+{
+	void maty();
+	static void mass()
 
-public class StaticInterface {
+	{
+		System.out.println("This is Maddy from static void");
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+}
+public class StaticInterface 
+{
+
+	public static void main(String[] args)
+	{
+		Maddy.mass();//You can directly call the method with class name when there is static in Interface 
 
 	}
 
